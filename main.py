11c3bd85e25f737959cf2eb6c51d56ec89af976a
Moritz Ruge <ruge.moritz@gmail.com>
2025-08-19 22:37:28 +0200

    added the function to create a note and store it in a specific path

--- main.py
+++ main.py
@@ -1,11 +1,42 @@
 import click
+import os
 
 def main():
-    @click.command()
-    @click.version_option("0.1.0", prog_name="cli-note")
-    def hello():
-        click.echo("Hello from cli-note!")
-    hello()
+
+
+    def menu():
+        print("1. New Note\n2. Exit\n")
+
+        user_input = int(input("Bitte wähle Sie die Funktion aus :)"))
+
+        match user_input:
+            case 1:
+                title = input("Bitte Titel eingeben: ")
+                text = input ("Text: ")
+                create_new_note(title, text)
+            case _:
+                exit()
+
+    def create_new_note(title, text):
+        filepath = os.path.join("./Note/", title)
+        if not os.path.exists("./Note/"):
+            os.makedirs("./Note/")
+        note = open(f"{filepath}.txt", "a") # "a" filepath erstellt die Datei schon
+        note.write(text)
+
+    def open_note():
+        pass
+    def delete_note():
+        pass
+    def list_all_notes():
+        pass
+
+
+    ### MAIN ###
+    menu()
+    
+
+
 
 
 if __name__ == "__main__":
